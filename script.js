f696b953686f468585cfad45a0d40cc2856bb46a
DonBlaSeP <dvivenegas.25@gmail.com>
2022-07-17 23:30:19 -0400

    html css formatter

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function color() {
-	var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-	document.body.style.backgroundColor = "#" + randomColor;
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
     document.getElementById("output").innerHTML = "Color actual: " + "#" + randomColor;
 }
 
-$("#btn").click(function() {
-	color();
+$("#btn").click(function () {
+    color();
 })
